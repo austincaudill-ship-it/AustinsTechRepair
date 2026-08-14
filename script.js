000d246e56fb04ab3624955790f7a932e4fc8070
austincaudill-ship-it <austincaudill@icloud.com>
2026-08-14 15:11:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -230,8 +230,7 @@ document.addEventListener('DOMContentLoaded', () => {
       dot.addEventListener('click', () => {
         currentSlide = index;
         showSlide(currentSlide);
-      });
     });
   }
-});
- 
+      });
+  }
